b8d9216e7d493d71f53413e78a1998ed20c4126a
Antonio Mesquita <antonio.mesquita@ifood.com.br>
2023-11-15 13:06:27 -0300

    feat: find todoitem by ID

--- todoitem/urls.py
+++ todoitem/urls.py
@@ -4,4 +4,5 @@ from todoitem import views
 
 urlpatterns = [
     path("", views.TodoitemView.as_view(), name="todoitem"),
+    path("/<id_arg>", views.TodoitemView.as_view(), name="todoitem"),
 ]
--- todoitem/views.py
+++ todoitem/views.py
@@ -7,6 +7,14 @@ from todoitem.serializers import TodoitemSerializer
 
 
 # Create your views here.
+def single(id_arg):
+    try:
+        queryset = Todoitem.objects.get(id=id_arg)
+        return queryset
+    except Todoitem.DoesNotExist:
+        return None
+
+
 class TodoitemView(APIView):
     def get(self, request):
         queryset = Todoitem.objects.all().order_by('created_at')
@@ -14,4 +22,20 @@ class TodoitemView(APIView):
         return Response(serializer.data)
 
     def post(self, request):
-        return Response({'msg': 'Resposta do método POST'}, status.HTTP_200_OK)
+
+        # Mocked author
+        request.data['author'] = 1
+        serializer = TodoitemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, id_arg):
+        queryset = single(id_arg=id_arg)
+        if queryset:
+            serializer = TodoitemSerializer(queryset)
+            return Response(serializer.data)
+        else:
+            return Response({'msg': f'Item com id #{id_arg} não encontrado'}, status.HTTP_404_NOT_FOUND)
