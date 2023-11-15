904344d55a9809957ce027e49badddd48f08a65d
Antonio Mesquita <antonio.mesquita@ifood.com.br>
2023-11-15 14:08:09 -0300

    feat: list and edit todo item

--- todoitem/urls.py
+++ todoitem/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from todoitem import views
 
 urlpatterns = [
+    path("/list", views.TodoitemsView.as_view(), name="todoitem-list"),
     path("", views.TodoitemView.as_view(), name="todoitem"),
     path("/<id_arg>", views.TodoitemView.as_view(), name="todoitem"),
 ]
--- todoitem/views.py
+++ todoitem/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -16,15 +18,15 @@ def single(id_arg):
 
 
 class TodoitemView(APIView):
-    def get(self, request):
-        queryset = Todoitem.objects.all().order_by('created_at')
-        serializer = TodoitemSerializer(queryset, many=True)
-        return Response(serializer.data)
-
     def post(self, request):
 
         # Mocked author
         request.data['author'] = 1
+
+        # Set create_at and updated_at time now()
+        request.data['created_at'] = datetime.now()
+        request.data['updated_at'] = datetime.now()
+
         serializer = TodoitemSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -39,3 +41,31 @@ class TodoitemView(APIView):
             return Response(serializer.data)
         else:
             return Response({'msg': f'Item com id #{id_arg} não encontrado'}, status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, id_arg):
+
+        # Find todoitem to update
+        todoitem = single(id_arg)
+        if todoitem is None:
+            return Response({'msg': f'Item com id #{id_arg} não encontrado'}, status.HTTP_404_NOT_FOUND)
+
+        # Mocked author
+        request.data['author'] = 1
+
+        request.data['updated_at'] = datetime.utcnow()
+        request.data['created_at'] = todoitem.created_at
+
+        serializer = TodoitemSerializer(todoitem, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+class TodoitemsView(APIView):
+    def get(self, request):
+        queryset = Todoitem.objects.all().order_by('created_at')
+        serializer = TodoitemSerializer(queryset, many=True)
+        return Response(serializer.data)
+
